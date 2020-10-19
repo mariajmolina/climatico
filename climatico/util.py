096fbc293d24931aafaebba08dece594f6694866
Maria Molina <hurricanemolina@gmail.com>
2020-10-19 02:13:57 -0600

    added weighted mean

--- climatico/util.py
+++ climatico/util.py
@@ -1,3 +1,6 @@
+import numpy as np
+import xarray as xr
+
 def pacific_lon(longitude, to180=True):
     """
     Help converting Pacific longitudes to 180 or 360 coordinates.
@@ -16,3 +19,20 @@ def pacific_lon(longitude, to180=True):
             return longitude+360
         if longitude >= 0:
             return longitude
+
+def weighted_mean(da, lat_name):
+    """
+    Compute the weighted mean of data.
+    Args:
+        da (xarray data array): Some data. Make sure to reduce coordinates to ``time, lat, lon``.
+        lat_name (str): Name of array containing latitudes for weights. Can be multi-dimensional.
+    Returns:
+        weighted (area averaged) mean of variable.
+    """
+    weights = np.cos(np.deg2rad(da.coords[lat_name]))
+    weights.name = "weights"
+    sst_weighted = da.weighted(weights)
+    # make sure dims are in time, then lat/lons for next step!
+    weighted_mean = sst_weighted.mean(dim=(da.dims[1:3]), skipna=True)
+    # returns weighted mean with one data value per time
+    return weighted_mean
