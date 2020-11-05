8728b0f438c000870122bcb0d2c71b0b8e6b5832
Maria Molina <hurricanemolina@gmail.com>
2020-11-04 17:47:33 -0700

    creation of mfds preprocess nino func

--- climatico/enso.py
+++ climatico/enso.py
@@ -3,6 +3,7 @@ import xarray as xr
 import pandas as pd
 from util import pacific_lon, weighted_mean
 import matplotlib.pyplot as plt
+import warnings
 
 class DefineNino:
     """
@@ -12,14 +13,26 @@ class DefineNino:
                     Options include nino12, nino3, nino34, nino4, atl3.
         lats (str): Name of regular/irregular grid latitudes (should have two dims). Defaults to ``TLAT``.
         lons (str): Name of regular/irregular grid longitudes (should have two dims). Defaults to ``TLONG``.
+        enso_event (str): ``nino``, ``nina``, or ``neutral`` events based on ``cutoff``. Defaults to ``None``.
+        cutoff (float): The +/- nino threshold. Defaults to ``0.5`` for ONI.
+                        Use ``0.5`` for nino sst regions.
+        strong_cutoff (float): The +/- nino index threshold for strong events. Defaults to ``1.5``.
+        runningmean (int): Final running mean for index. Defaults to ``3``.
+                           Select 5 for Nino regions and 3 for ONI.
     """
-    def __init__(self, nino, lats='TLAT', lons='TLONG'):
+    def __init__(self, nino, sst_name='SST', lats='TLAT', lons='TLONG', enso_event=None, 
+                 cutoff=0.5, strong_cutoff=1.5, runningmean=3):
         """
         Initialize.
         """
         self.ninodefined = nino
+        self.sst_name = sst_name
         self.lats = lats
         self.lons = lons
+        self.enso_event = enso_event
+        self.cutoff = cutoff
+        self.strong_cutoff = strong_cutoff
+        self.runningmean = runningmean
 
     def nino_dict(self):
         """
@@ -77,37 +90,63 @@ class DefineNino:
         Returns:
             nino_climo with rolling mean computed along months.
         """
-        jan = self.roll_climo(data, month=1, yrsroll=yrsroll, centered=centered, time=time)
-        feb = self.roll_climo(data, month=2, yrsroll=yrsroll, centered=centered, time=time)
-        mar = self.roll_climo(data, month=3, yrsroll=yrsroll, centered=centered, time=time)
-        apr = self.roll_climo(data, month=4, yrsroll=yrsroll, centered=centered, time=time)
-        may = self.roll_climo(data, month=5, yrsroll=yrsroll, centered=centered, time=time)
-        jun = self.roll_climo(data, month=6, yrsroll=yrsroll, centered=centered, time=time)
-        jul = self.roll_climo(data, month=7, yrsroll=yrsroll, centered=centered, time=time)
-        aug = self.roll_climo(data, month=8, yrsroll=yrsroll, centered=centered, time=time)
-        sep = self.roll_climo(data, month=9, yrsroll=yrsroll, centered=centered, time=time)
-        boo = self.roll_climo(data, month=10, yrsroll=yrsroll, centered=centered, time=time)
-        nov = self.roll_climo(data, month=11, yrsroll=yrsroll, centered=centered, time=time)
-        dec = self.roll_climo(data, month=12, yrsroll=yrsroll, centered=centered, time=time)
-        nino_climo = xr.concat([jan,feb,mar,apr,may,jun,jul,aug,sep,boo,nov,dec], dim=time).sortby(time)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            jan = self.roll_climo(data, month=1, yrsroll=yrsroll, centered=centered, time=time)
+            feb = self.roll_climo(data, month=2, yrsroll=yrsroll, centered=centered, time=time)
+            mar = self.roll_climo(data, month=3, yrsroll=yrsroll, centered=centered, time=time)
+            apr = self.roll_climo(data, month=4, yrsroll=yrsroll, centered=centered, time=time)
+            may = self.roll_climo(data, month=5, yrsroll=yrsroll, centered=centered, time=time)
+            jun = self.roll_climo(data, month=6, yrsroll=yrsroll, centered=centered, time=time)
+            jul = self.roll_climo(data, month=7, yrsroll=yrsroll, centered=centered, time=time)
+            aug = self.roll_climo(data, month=8, yrsroll=yrsroll, centered=centered, time=time)
+            sep = self.roll_climo(data, month=9, yrsroll=yrsroll, centered=centered, time=time)
+            boo = self.roll_climo(data, month=10, yrsroll=yrsroll, centered=centered, time=time)
+            nov = self.roll_climo(data, month=11, yrsroll=yrsroll, centered=centered, time=time)
+            dec = self.roll_climo(data, month=12, yrsroll=yrsroll, centered=centered, time=time)
+            nino_climo = xr.concat([jan,feb,mar,apr,may,jun,jul,aug,sep,boo,nov,dec], dim=time).sortby(time)
         return nino_climo
     
-    def compute_index(self, data, climo, runningmean=3):
+    def compute_index(self, data, climo):
         """
         Compute nino index (sst based).
         Args:
             data (xarray data array): Weighted mean variable.
             climo (xarray data array): Monthly climatology.
-            runningmean (int): Final running mean for index. Defaults to ``3``.
-                               Select 5 for Nino regions and 3 for ONI.
         """
         anom = data - climo
-        if runningmean == 5:
+        if self.runningmean == 5:
             anom = anom.chunk({'time': 12})
-        anom_rolling = anom.rolling(time=runningmean, min_periods=1, center=False).mean()
+        anom_rolling = anom.rolling(time=self.runningmean, min_periods=1, center=False).mean()
         std = anom_rolling.std()
         nino_index = (anom_rolling / std)
         return nino_index
+
+    def grab_nino(self, ds, squeeze=True):
+        """
+        Extract nino sst regions that were enso or not.
+        Args:
+            ds (xarray dataset): SST data.
+            squeeze (boolean): Whether to squeeze out any len == 1 dims. Defaults to ``True``.
+        """
+        ds = self.nino(ds)
+        if squeeze:
+            da = ds.squeeze()
+        # compute weighted mean of sst region
+        nino_mean = weighted_mean(da[self.sst_name], lat_name=self.lats)
+        # compute monthly climo (running centered 30-yr mean)
+        climo = self.monthly_climo(nino_mean, yrsroll=30, centered=True)
+        # compute the respective nino index
+        index = self.compute_index(data=nino_mean, climo=climo)
+        index = index.values
+        # extract enso events
+        if enso_event == "nino":
+            enso_events = da[self.sst_name].isel(time=np.where(index>=self.cutoff)[0])
+        if enso_event == "nina":
+            enso_events = da[self.sst_name].isel(time=np.where(index<=-self.cutoff)[0])
+        if enso_event == "neutral":
+            enso_events = da[self.sst_name].isel(time=np.where((index<self.cutoff)&(index>-self.cutoff))[0])
+        return enso_events
 
     def check_nino(self, data):
         """
@@ -121,93 +160,67 @@ class DefineNino:
         print('Longitude range: {:.1f} - {:.1f}'.format(pacific_lon(data[self.lons].min().values), 
                                                         pacific_lon(data[self.lons].max().values)))
 
-    def check_nino_percentages(self, index, cutoff=0.4):
+    def check_nino_percentages(self, index):
         """
         Check percent of data set that contains nino and nina events.
         Args:
             index (numpy array): Index eagerly loaded as numpy array (for speed).
-            cutoff (float): The +/- nino threshold. Defaults to ``0.4`` for nino sst regions.
-                            Use ``0.5`` for oni.
         """
-        # previous code for xarray data array option
-        # print("Percentage of El Nino events = {:0.1f}%".format(100 * (index.where(index>=cutoff).count() / index.count()).values))
-        # print("Percentage of La Nina events = {:0.1f}%".format(100 * (index.where(index<=-cutoff).count() / index.count()).values))
         print("Percentage of El Nino events = {:0.1f}%".format(
-            100 * np.around(np.count_nonzero((np.where(index>=cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
+            100 * np.around(np.count_nonzero((np.where(index>=self.cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
         print("Percentage of La Nina events = {:0.1f}%".format(
-            100 * np.around(np.count_nonzero((np.where(index<=-cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
+            100 * np.around(np.count_nonzero((np.where(index<=-self.cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
 
-    def check_strong_nino(self, index, cutoff=0.4, strong_cutoff=1.5):
+    def check_strong_nino(self, index):
         """
         Check percent of data set that contains nino and nina events.
         Args:
             index (numpy array): Index eagerly loaded as numpy array (for speed).
-            cutoff (float): The +/- nino threshold. Defaults to ``0.4`` for nino sst regions.
-                            Use ``0.5`` for oni.
-            strong_cutoff (float): The +/- strong nino threshold. Defaults to ``1.5``.
         """
-        # previous code for xarray data array option
-        # print("Percentage of Strong El Nino events = {:0.1f}%".format(
-        #     100 * (index.where(index>=strong_cutoff).count() / np.count_nonzero(~np.isnan(index))).values))
-        # print("Percentage of Strong La Nina events = {:0.1f}%".format(
-        #     100 * (index.where(index<=-strong_cutoff).count() / np.count_nonzero(~np.isnan(index))).values))
-        # print("Percentage of Strong El Nino from All El Nino = {:0.1f}%".format(
-        #     100 * (index.where(index>=strong_cutoff).count() / index.where(index>=cutoff).count()).values))
-        # print("Percentage of Strong La Nina from All La Nina = {:0.1f}%".format(
-        #     100 * (index.where(index<=-strong_cutoff).count() / index.where(index<=-cutoff).count()).values))
         print("Percentage of Strong El Nino events = {:0.1f}%".format(
-            100 * np.around(np.count_nonzero((np.where(index>=strong_cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
+            100 * np.around(np.count_nonzero((np.where(index>=self.strong_cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
         print("Percentage of Strong La Nina events = {:0.1f}%".format(
-            100 * np.around(np.count_nonzero((np.where(index<=-strong_cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
+            100 * np.around(np.count_nonzero((np.where(index<=-self.strong_cutoff, 1, 0))) / np.count_nonzero(~np.isnan(index)),2)))
         print("Percentage of Strong El Nino from All El Nino = {:0.1f}%".format(
-            100 * (np.count_nonzero((np.where(index>=strong_cutoff, 1, 0))) / np.count_nonzero((np.where(index>=cutoff, 1, 0))))))
+            100 * (np.count_nonzero((np.where(index>=self.strong_cutoff, 1, 0))) / np.count_nonzero((np.where(index>=self.cutoff, 1, 0))))))
         print("Percentage of Strong La Nina from All La Nina = {:0.1f}%".format(
-            100 * (np.count_nonzero((np.where(index<=-strong_cutoff, 1, 0))) / np.count_nonzero((np.where(index<=-cutoff, 1, 0))))))
+            100 * (np.count_nonzero((np.where(index<=-self.strong_cutoff, 1, 0))) / np.count_nonzero((np.where(index<=-self.cutoff, 1, 0))))))
 
-    def fast_plot(self, index, cutoff=0.4):
+    def fast_plot(self, index):
         """
         Quick visualization of index.
         Args:
             index (numpy array): Index eagerly loaded as numpy array (for speed).
-            cutoff (float): The +/- nino threshold. Defaults to ``0.4`` for nino sst regions.
-                            Use ``0.5`` for oni.
         """
-        # previous code for xarray data array
-        # index.plot(size=12)
         plt.plot(index)
         plt.margins(x=0)
         plt.axhline(0,color='black',lw=0.5)
-        plt.axhline(cutoff,color='black',linewidth=0.5,linestyle='dotted')
-        plt.axhline(-cutoff,color='black',linewidth=0.5,linestyle='dotted')
+        plt.axhline(self.cutoff,color='black',linewidth=0.5,linestyle='dotted')
+        plt.axhline(-self.cutoff,color='black',linewidth=0.5,linestyle='dotted')
         plt.xlabel('Months')
         plt.ylabel('Index')
         plt.show()
 
-    def shaded_plot(self, index, cutoff=0.4, strong_cutoff=1.5, title=None, savefig=None,
+    def shaded_plot(self, index, title=None, savefig=None,
                     xticks=[0,1800,3600,5400,7200,9000], xticklabels=[0,150,300,450,600,750]):
         """
         Quick shaded visualization of index.
         Args:
             index (numpy array): Index eagerly loaded as numpy array (for speed).
-            cutoff (float): The +/- nino threshold. Defaults to ``0.4`` for nino sst regions.
-                            Use ``0.5`` for oni.
             title (str): Title for the figure.
             savefig (str): Directory and figure name to save.
             xticks (list): X ticks. Defaults to freshwater hosing experiment time.
             xticklabels (list): X tick labels. Defaults to freshwater hosing experiment time.
         """
         fig = plt.figure(figsize=(12, 8))
-        # plt.fill_between(index.time.values, index.where(index>=cutoff).values, cutoff, color='r', alpha=0.8)
-        # plt.fill_between(index.time.values, index.where(index<=-cutoff).values, -cutoff, color='b', alpha=0.3)
-        # index.plot(color='black', lw=0.2)
-        plt.fill_between(range(index.shape[0]), np.where(index>=cutoff,index,np.nan), cutoff, color='r', alpha=0.8); 
-        plt.fill_between(range(index.shape[0]), np.where(index<=-cutoff,index,np.nan), -cutoff, color='b', alpha=0.3)
+        plt.fill_between(range(index.shape[0]), np.where(index>=self.cutoff,index,np.nan), self.cutoff, color='r', alpha=0.8); 
+        plt.fill_between(range(index.shape[0]), np.where(index<=-self.cutoff,index,np.nan), -self.cutoff, color='b', alpha=0.3)
         plt.plot(index, c='k', lw=0.15)
         plt.axhline(0,color='black',lw=0.5)
-        plt.axhline(cutoff,color='black',linewidth=0.5,linestyle='dotted')
-        plt.axhline(-cutoff,color='black',linewidth=0.5,linestyle='dotted')
-        plt.axhline(strong_cutoff,color='black',linewidth=0.25,linestyle='dotted')
-        plt.axhline(-strong_cutoff,color='black',linewidth=0.25,linestyle='dotted')
+        plt.axhline(self.cutoff,color='black',linewidth=0.5,linestyle='dotted')
+        plt.axhline(-self.cutoff,color='black',linewidth=0.5,linestyle='dotted')
+        plt.axhline(self.strong_cutoff,color='black',linewidth=0.25,linestyle='dotted')
+        plt.axhline(-self.strong_cutoff,color='black',linewidth=0.25,linestyle='dotted')
         plt.xticks(xticks, xticklabels)
         plt.xlabel('Years')
         plt.ylabel('Index')
@@ -220,14 +233,12 @@ class DefineNino:
         if not savefig:
             return plt.show()
 
-    def nino_cumsum(self, index, cutoff=0.4, title=None, savefig=None, 
+    def nino_cumsum(self, index, title=None, savefig=None, 
                     xticks=[0,1800,3600,5400,7200,9000], xticklabels=[0,150,300,450,600,750]):
         """
         Cumulative sum of El Nino, La Nino, and Neutral events over the index data.
         Args:
             index (numpy array): Index eagerly loaded as numpy array (for speed).
-            cutoff (float): The +/- nino threshold. Defaults to ``0.4`` for nino sst regions.
-                            Use ``0.5`` for oni.
             title (str): Title for the figure. Defaults to None.
             savefig (str): Directory and figure name to save. Defaults to None.
             xticks (list): X ticks. Defaults to freshwater hosing experiment time.
@@ -235,13 +246,9 @@ class DefineNino:
         """
         fig = plt.figure(figsize=(10, 6))
         ax = plt.axes([0.,0.,1.,1.])
-        # previous xarray data array
-        # index.where(index>=cutoff, 0).where(index<cutoff, 1).cumsum(dim='time').plot(ax=ax, c='r')
-        # index.where(index<=-cutoff, 0).where(index>-cutoff, 1).cumsum(dim='time').plot(ax=ax, c='b')
-        # index.where((index<cutoff)&(index>-cutoff), 0).where((index>=cutoff)|(index<=-cutoff), 1).cumsum(dim='time').plot(ax=ax, c='k')
-        ax.plot(np.cumsum(np.where(index>=cutoff, 1, 0)), c='r')
-        ax.plot(np.cumsum(np.where(index<=-cutoff, 1, 0)), c='b')
-        ax.plot(np.cumsum(np.where((index<cutoff)&(index>-cutoff), 1, 0)), c='k')
+        ax.plot(np.cumsum(np.where(index>=self.cutoff, 1, 0)), c='r')
+        ax.plot(np.cumsum(np.where(index<=-self.cutoff, 1, 0)), c='b')
+        ax.plot(np.cumsum(np.where((index<self.cutoff)&(index>-self.cutoff), 1, 0)), c='k')
         ax.set_xticks(xticks)
         ax.set_xticklabels(xticklabels)
         ax.set_xlabel('Years')
