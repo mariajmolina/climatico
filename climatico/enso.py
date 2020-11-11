50d92f4906e7133a5cdd9424df8de182e1380da6
Maria Molina <hurricanemolina@gmail.com>
2020-11-10 21:38:25 -0700

    improve and clean up docstrings

--- climatico/enso.py
+++ climatico/enso.py
@@ -11,13 +11,15 @@ class DefineNino:
     Args:
         nino (str): Nino SST region in Pacific (all lowercase). E.g., ``nino34``.
                     Options include nino12, nino3, nino34, nino4, atl3.
+        sst_name (str): The name of the SST variable in the file. Defaults to ``SST``.
         lats (str): Name of regular/irregular grid latitudes (should have two dims). Defaults to ``TLAT``.
         lons (str): Name of regular/irregular grid longitudes (should have two dims). Defaults to ``TLONG``.
-        enso_event (str): ``nino``, ``nina``, or ``neutral`` events based on ``cutoff``. Defaults to ``None``.
+        enso_event (str): Needed for grab_nino function. ``nino``, ``nina``, or ``neutral`` events based on 
+                          ``cutoff`` value. Defaults to ``None``.
         cutoff (float): The +/- nino threshold. Defaults to ``0.5`` for ONI.
-                        Use ``0.5`` for nino sst regions.
+                        Use ``0.4`` for just nino sst regions (not ONI).
         strong_cutoff (float): The +/- nino index threshold for strong events. Defaults to ``1.5``.
-        runningmean (int): Final running mean for index. Defaults to ``3``.
+        runningmean (int): Final running mean for index computation (after anomalies). Defaults to ``3``.
                            Select 5 for Nino regions and 3 for ONI.
     """
     def __init__(self, nino, sst_name='SST', lats='TLAT', lons='TLONG', enso_event=None, 
@@ -91,6 +93,7 @@ class DefineNino:
             nino_climo with rolling mean computed along months.
         """
         with warnings.catch_warnings():
+            # ignore computer performance warning here on chunks
             warnings.simplefilter("ignore")
             jan = self.roll_climo(data, month=1, yrsroll=yrsroll, centered=centered, time=time)
             feb = self.roll_climo(data, month=2, yrsroll=yrsroll, centered=centered, time=time)
@@ -107,12 +110,11 @@ class DefineNino:
             nino_climo = xr.concat([jan,feb,mar,apr,may,jun,jul,aug,sep,boo,nov,dec], dim=time).sortby(time)
         return nino_climo
     
-    def monthly_climo_control(self, data, time='time'):
+    def monthly_climo_control(self, data):
         """
         Create fixed mean climatology.
         Args:
             data (xarray data array): Weighted mean variable.
-            time (str): Time coordinate name. Defaults to ``time``.
         Returns:
             Fixed period weighted mean climatology.
         """
@@ -125,11 +127,16 @@ class DefineNino:
             data (xarray data array): Weighted mean variable.
             climo (xarray data array): Monthly climatology.
         """
+        # create anomalies
         anom = data - climo
         if self.runningmean == 5:
+            # for performance
             anom = anom.chunk({'time': 12})
+        # apply running mean onto anomalies
         anom_rolling = anom.rolling(time=self.runningmean, min_periods=1, center=False).mean()
+        # extract standard deviation
         std = anom_rolling.std()
+        # standardize rolled anoms
         nino_index = (anom_rolling / std)
         return nino_index
 
@@ -140,6 +147,7 @@ class DefineNino:
             ds (xarray dataset): SST data.
             squeeze (boolean): Whether to squeeze out any len == 1 dims. Defaults to ``True``.
         """
+        # slice sst data for respective nino region
         ds = self.nino(ds)
         if squeeze:
             da = ds.squeeze()
@@ -149,6 +157,7 @@ class DefineNino:
         climo = self.monthly_climo(nino_mean, yrsroll=30, centered=True)
         # compute the respective nino index
         index = self.compute_index(data=nino_mean, climo=climo)
+        # eagerly load data for more speed later
         index = index.values
         # extract enso events
         if self.enso_event == "nino":
@@ -184,7 +193,7 @@ class DefineNino:
 
     def check_strong_nino(self, index):
         """
-        Check percent of data set that contains nino and nina events.
+        Check percent of data set that contains strong nino and nina events.
         Args:
             index (numpy array): Index eagerly loaded as numpy array (for speed).
         """
@@ -227,18 +236,28 @@ class DefineNino:
                                   Defaults to ``None``. Should be e.g., ``[6000]``.
         """
         fig = plt.figure(figsize=(12, 8))
-        plt.fill_between(range(index.shape[0]), np.where(index>=self.cutoff,index,np.nan), self.cutoff, color='r', alpha=0.8); 
+        # nino shade
+        plt.fill_between(range(index.shape[0]), np.where(index>=self.cutoff,index,np.nan), self.cutoff, color='r', alpha=0.8)
+        # nina shade
         plt.fill_between(range(index.shape[0]), np.where(index<=-self.cutoff,index,np.nan), -self.cutoff, color='b', alpha=0.3)
+        # plot index
         plt.plot(index, c='k', lw=0.15)
+        # plot 0 anom line (horizontal)
         plt.axhline(0,color='black',lw=0.5)
+        # plot nino threshold
         plt.axhline(self.cutoff,color='black',linewidth=0.5,linestyle='dotted')
+        # plot nina threshold
         plt.axhline(-self.cutoff,color='black',linewidth=0.5,linestyle='dotted')
+        # plot strong nino threshold
         plt.axhline(self.strong_cutoff,color='black',linewidth=0.25,linestyle='dotted')
+        # plot strong nina threshold
         plt.axhline(-self.strong_cutoff,color='black',linewidth=0.25,linestyle='dotted')
+        # plot labels
         plt.xticks(xticks, xticklabels)
         plt.xlabel('Years')
         plt.ylabel('Index')
         if add_yr_lines:
+            # adding vertical lines 
             for flux in add_yr_lines:
                 plt.axvline(flux, color='k', lw=1.5)
         if title:
@@ -266,14 +285,19 @@ class DefineNino:
         """
         fig = plt.figure(figsize=(10, 6))
         ax = plt.axes([0.,0.,1.,1.])
+        # nino cumulative sum
         ax.plot(np.cumsum(np.where(index>=self.cutoff, 1, 0)), c='r')
+        # nina cumulative sum
         ax.plot(np.cumsum(np.where(index<=-self.cutoff, 1, 0)), c='b')
+        # neutral cumulative sum
         ax.plot(np.cumsum(np.where((index<self.cutoff)&(index>-self.cutoff), 1, 0)), c='k')
+        # labels
         ax.set_xticks(xticks)
         ax.set_xticklabels(xticklabels)
         ax.set_xlabel('Years')
         ax.set_ylabel('Index')
         if add_yr_lines:
+            # vertical lines
             for flux in add_yr_lines:
                 ax.axvline(flux, color='k', lw=1.5)
         if title:
--- climatico/util.py
+++ climatico/util.py
@@ -6,8 +6,8 @@ def pacific_lon(longitude, to180=True):
     Help converting Pacific longitudes to 180 or 360 coordinates.
     
     Args:
-        longitude (float): Single longitude value.
-        to180 (boolean): If coords are in 360 degrees. Defaults to ``True``.
+        longitude (float): Longitude value.
+        to180 (boolean): True if longitudes are in 360 degrees. Defaults to ``True``.
     """
     if to180:
         if longitude <= 180:
